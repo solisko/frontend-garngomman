47a9e9f82496d08d45ddcba510f3dff49ccf7350
Sofia Skommar <sofiaskommar@gmail.com>
2024-10-01 12:25:46 +0200

    trying to import, not working still

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import "./App.css";
 import { Header } from "@solisko/components-garngomman";
+import { Start } from "@solisko/components-garngomman";
 
 function App() {
-  const FetchFromBackEnd = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/api/user");
+  // const FetchFromBackEnd = async () => {
+  //   try {
+  //     const response = await fetch("http://localhost:3000/api/user");
 
-      if (!response.ok) {
-        throw new Error("Wtf");
-      }
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error("catchen");
-    }
-  };
+  //     if (!response.ok) {
+  //       throw new Error("Wtf");
+  //     }
+  //     const data = await response.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.error("catchen");
+  //   }
+  // };
 
-  FetchFromBackEnd();
+  // FetchFromBackEnd();
 
   return (
     <>
       <Header />
+      <Start />
     </>
   );
 }
