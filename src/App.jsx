1ec16a4670d60ab6e4c3f45e486f402b0c0246a2
Sofia Skommar <sofiaskommar@gmail.com>
2024-10-01 11:50:13 +0200

    solisko pkg install

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,22 @@ import "./App.css";
 import { Header } from "@solisko/components-garngomman";
 
 function App() {
+  const FetchFromBackEnd = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/user");
+
+      if (!response.ok) {
+        throw new Error("Wtf");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("catchen");
+    }
+  };
+
+  FetchFromBackEnd();
+
   return (
     <>
       <Header />
