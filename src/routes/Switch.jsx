78810f78e2d7a9b0c50afd81dccf34bdadda969c
Nellie Gränd <nellie.grand@hotmail.com>
2024-10-06 11:07:34 +0200

    added paths for profile

--- src/routes/Switch.jsx
+++ src/routes/Switch.jsx
@@ -1,4 +1,14 @@
-import { Start, Community } from "@solisko/components-garngomman";
+import {
+  Start,
+  Community,
+  Profil,
+  Gdpr,
+  /* accountSettings, */
+  Membership,
+  PersonSettings,
+  Preferences,
+  Support,
+} from "@solisko/components-garngomman";
 import { Routes, Route } from "react-router";
 import StashComp from "../components/StashComp";
 
@@ -10,13 +20,13 @@ const Switch = () => {
         <Route path="/login" />
         <Route path="/registration" />
         <Route path="/stash" element={<StashComp />} />
-        <Route path="/profile" />
-        <Route path="/profile/gdpr" />
-        <Route path="/profile/accountsettings" />
-        <Route path="/profile/membership" />
-        <Route path="/profile/personsettings" />
-        <Route path="/profile/preferences" />
-        <Route path="/profile/support" />
+        <Route path="/Profile" element={<Profil />} />
+        <Route path="/profile/gdpr" element={<Gdpr />} />
+        {/*         <Route path="/profile/accountsettings" element={<accountSettings />} /> */}
+        <Route path="/profile/membership" element={<Membership />} />
+        <Route path="/profile/personsettings" element={<PersonSettings />} />
+        <Route path="/profile/preferences" element={<Preferences />} />
+        <Route path="/profile/support" element={<Support />} />
         <Route path="/addtostah" />
         <Route path="/project" />
         <Route path="/community" element={<Community />} />
