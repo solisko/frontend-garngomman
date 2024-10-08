2f13bd3d3e6641bfd4830b232797b79d68303f84
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-10-08 22:07:40 +0200

    updated pkg version

--- src/context/GarnProvider.jsx
+++ src/context/GarnProvider.jsx
@@ -1,12 +1,35 @@
 import { createContext, useState } from "react";
+import {
+  AdNewNeedleMobile,
+  AdNewYarnMobile,
+  AdNewAccesoryMobile,
+} from "@solisko/components-garngomman";
 
 export const GarnContext = createContext();
 
 const GarnProvider = (props) => {
   const [category, setCategory] = useState(null);
+  const [renderdComponent, setRenderdComponent] = useState(null);
 
   const categorySetter = (title) => {
     setCategory(title);
+  };
+
+  const renderComponent = (category) => {
+    switch (category) {
+      case "Tillbehör":
+        setRenderdComponent(<AdNewAccesoryMobile />);
+        break;
+      case "Nålar":
+        setRenderdComponent(<AdNewNeedleMobile />);
+        break;
+      case "Garn":
+        setRenderdComponent(<AdNewYarnMobile />);
+        break;
+      default:
+        setRenderdComponent(null);
+        break;
+    }
   };
 
   return (
@@ -15,6 +38,8 @@ const GarnProvider = (props) => {
         value={{
           categorySetter,
           category,
+          renderdComponent,
+          renderComponent,
         }}
       >
         {props.children}
